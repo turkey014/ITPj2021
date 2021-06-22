0915ccb625920b7ed698218a81cde3b1897347c0
Your Name <you@example.com>
2021-06-22 15:35:39 +0900

    changing home

--- public/home.php
+++ public/home.php
@@ -5,7 +5,26 @@ require_once(__DIR__ . '/init.php');
 
 // 
 $template_filename = 'home.twig';
+
+/*
 $context = [];
 
+//出力
+require_once(BASEPATH . '/public/fin.php');
+*/
+
+$year = date('Y');
+$month = date('m');
+$weeks = ['日','月','火','水','木','金','土'];
+$end_month = date('t', strtotime($year.$month.'01')); // t = 月の日数
+$first_month = date('w', strtotime($year.$month.'01')); // w = 初日の曜日
+//$f = $first_month;
+//$br = 7 - $first_month;
+
+
+$context = [
+	'cal' => [$year, $month, $weeks, $end_month, $first_month],
+];
+var_dump($context);
 //出力
 require_once(BASEPATH . '/public/fin.php');
