9008e746b5f07a3472242e906a6371a48b9a9770
Your Name <you@example.com>
2021-07-13 15:03:44 +0900

    send date to dayresult on 0713

--- public/day_result.php
+++ public/day_result.php
@@ -6,16 +6,39 @@ require_once(__DIR__ . '/init.php');
 // 
 $template_filename = 'day_result.twig';
 
-
+// 閲覧する日付の取得
 if (isset($_GET['ymd'])) {
-    $ymd = strtotime($_GET['ymd']);
+    $date = strtotime($_GET['ymd']);
 } else {
     // 今月の年月を表示
     echo "errer";	
 }
-$month = date('m', $ymd);
-$day = date('d', $ymd);
+$month = date('m', $date);
+$day = date('d', $date);
 
+
+//DBアクセス
+
+/*
+//testデータ
+$user_id = 1;
+
+
+
+$r = $dbh->beginTransaction();
+//register から回収
+$sql = 'SELECT * FROM register WHERE user_id=:user_id AND date=:date;';
+$pre = $dbh->prepare($sql);
+var_dump($pre);
+
+// プレースホルダに値をバインド
+$pre->bindValue(':user_id', $user_id);
+$pre->bindValue(':date', $date);
+
+// sql を実行
+//$r = $pre->execute();
+//var_dump($r);
+*/
 
 
 $context = [
