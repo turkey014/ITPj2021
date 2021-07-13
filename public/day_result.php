82e903d0c33e0f91bf2e350bacf41ecaa1958a22
Your Name <you@example.com>
2021-07-13 14:21:55 +0900

    make calender

--- public/day_result.php
+++ public/day_result.php
@@ -5,7 +5,23 @@ require_once(__DIR__ . '/init.php');
 
 // 
 $template_filename = 'day_result.twig';
-$context = [];
+
+
+if (isset($_GET['ymd'])) {
+    $ymd = strtotime($_GET['ymd']);
+} else {
+    // 今月の年月を表示
+    echo "errer";	
+}
+$month = date('m', $ymd);
+$day = date('d', $ymd);
+
+
+
+$context = [
+	'month' => $month,
+	'day' => $day,
+];
 
 //出力
 require_once(BASEPATH . '/public/fin.php');
