1f7dd2e9b1b351ffd239c17451762da896e615c4
Your Name <you@example.com>
2022-01-18 18:31:25 +0900

    model update

--- public/day_result.php
+++ public/day_result.php
@@ -12,7 +12,7 @@ if (isset($_GET['ymd'])) {
     $date = $_GET['ymd'];
 } else {
     // 今月の年月を表示
-    echo "errer";	
+    echo "error";	
 }
 $month = date('m', strtotime($date));
 $day = date('d', strtotime($date));
@@ -20,20 +20,13 @@ $day = date('d', strtotime($date));
 // test用
 $user_id = 2;
 
-$test = Modelmine::day_registers($user_id, $date);
-$array = $test->array();
-//var_dump($array);exit;
-/*
-foreach($array as $k => $v){
-	var_dump($k, $v);
-}
-*/
+$test = Modelmine::select_day($date);
 $sum = Modelmine::sums($test);
 
 $context = [
 	'month' => $month,
 	'day' => $day,
-	'contents' => $array,
+	'contents' => $test,
 	'sums' => $sum,
 ];
 
